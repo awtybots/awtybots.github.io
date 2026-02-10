4ce874fa55ca29c30e0b49636f665524a0bbe95c
AwtyBotsOfficial <guptai@awty.org>
2026-02-09 22:36:18 -0600

    Recaps
    Added prototype of meeting recaps

---
+++ assets/js/recaps.js
@@ -0,0 +1,79 @@
+let days = 
+[
+    0, 1, 3, 4, 5, 6, 7, 8, 11, 12, 13, 14, 15, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28, 29, 31
+];
+
+let day = days.length - 1;
+
+const nextBtn = document.getElementById('next');
+const backBtn = document.getElementById('back');
+
+const post = document.getElementById("post");
+
+async function loadRecap(day)
+{
+    const res = await fetch(`https://raw.githubusercontent.com/awtybots/awtybots.github.io/main/assets/recaps/Day ${day}.txt`);
+    if(!res.ok) console.log('fetch failed');
+
+    const text = await res.text();
+
+    return text;
+}
+
+function addPost(dayNum, text)
+{
+    const article = document.createElement('article');
+
+    const title = document.createElement('h2');
+
+    title.innerText =  `Day ${dayNum}`;
+    title.className = "display-5 link-body-emphasis mb-1";
+    title.id = "recap-post";
+    article.appendChild(title);
+    article.appendChild(document.createElement('hr'));
+
+    const rec = document.createElement('p');
+    rec.innerText = text;
+
+    article.appendChild(rec)
+    
+
+    post.replaceChildren(article);
+}
+
+nextBtn.addEventListener('click', async () => 
+{
+    if(day == days.length - 1)
+    {
+        day = days.length - 1;
+    }
+    else day++;
+
+    dayNum = days[day];
+
+    text = await loadRecap(dayNum);
+
+    addPost(dayNum, text);
+});
+
+backBtn.addEventListener('click', async () => 
+{
+    if(day == 0)
+    {
+        day = 0;
+    }
+    else day--;
+
+    dayNum = days[day];
+
+    text = await loadRecap(dayNum);
+
+    addPost(dayNum, text);
+});
+
+
+document.addEventListener('DOMContentLoaded', async function() {
+    text = await loadRecap(days[day]);
+
+    addPost(days[day], text);
+});
